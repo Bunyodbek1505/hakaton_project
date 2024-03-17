11de4437dccaad9cacdc28083a76b22fbe971c4a
Bunyodbek1505 <jorayevbunyodbek1505@gmail.com>
2024-03-17 12:47:43 -1100

    react-vite-project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './Button'
+import Login from './Login'
+
+function App() {
+
+  return (
+    <>
+      {/* <Button /> */}
+      <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './button.module.css'
+
+const Button = () => {
+  return (
+    <div className={style.foo}>Button</div>
+  )
+}
+
+export default Button
